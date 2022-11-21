0572aae1c781532342c61b6328e62082853a25e4
Iryna <ielkinairinka@gmail.com>
2022-11-21 17:34:24 +0300

    first commit

---
+++ src/js/main.js
@@ -0,0 +1,16 @@
+"use strict";
+$(function () {
+  $('.hero-slider__inner').slick({
+    dots: true,
+    arrows: false,
+    autoplay: true,
+    autoplaySpeed: 6000
+  })
+});
+
+// $(function () {
+//   // $('.hero-slider').slick({
+//   // dots: true,
+
+//   // });
+// })
