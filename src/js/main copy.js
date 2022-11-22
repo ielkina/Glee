a439607bacffcad2f9463e47e453fa5985174364
Iryna <ielkinairinka@gmail.com>
2022-11-22 15:06:04 +0300

    site

--- src/js/main copy.js
+++ src/js/main copy.js
@@ -40,3 +40,97 @@ $(function () {
 })
 
 
+const filterBox = document.querySelectorAll('.design-catalog');
+document.querySelector('.design-filter').addEventListener('click', event => {
+  if (event.target.tagName !== 'button') return false;
+  let filterClass = event.target.dataset['filter'];
+  filterBox.forEach(elem => {
+    elem.classList.remove('hide');
+    if (!elem.classList.contains(filterClass) && filterClass !== 'all') {
+      elem.classList.add('hide');
+    }
+  });
+});
+
+$(document).ready(function () {
+  var activeCat = "";
+  function filterGroup(group) {
+    if (activeCat != group) {
+      $("li").filter("." + group).show();
+      $("li").filter(":not(." + group + ")").hide();
+      activeCat = group;
+    }
+  }
+
+  $(".all").click(function () {
+    $("li").show();
+    activeCat = "all";
+  });
+  $(".furnitures").click(function () { filterGroup("furnitures"); });
+  $(".chairs").click(function () { filterGroup("chairs"); });
+  $(".lighting").click(function () { filterGroup("lighting"); });
+  $(".decor").click(function () { filterGroup("decor"); });
+});
+
+$(document).ready(function () {
+
+  var activeCat = "";
+  function filterGroup(group) {
+    if (activeCat != group) {
+      $("li").filter("." + group).show();
+      $("li").filter(":not(." + group + ")").hide();
+      activeCat = group;
+    }
+  }
+
+  $(".all").click(function () {
+    $("li").show();
+    activeCat = ".all";
+  });
+  $(".furnitures").click(function () { filterGroup("furnitures"); });
+  $(".chairs").click(function () { filterGroup("chairs"); });
+  $(".lighting").click(function () { filterGroup("lighting"); });
+  $(".decor").click(function () { filterGroup("decor"); });
+});
+
+let sortBtn = document.querySelector('.design-filter').children;
+let sortItem = document.querySelector('.design-catalog__item').children;
+for (let i = 0; i < sortBtn.length; i++) {
+  sortBtn[i].addEventListener('click', function () {
+    for (let j = 0; j < sortBtn.length; j++) {
+      sortBtn[j].classList.remove('current');
+    }
+
+    this.classList.add('current');
+
+
+    let targetData = this.getAttribute('data-target');
+
+    for (let k = 0; k < sortItem.length; k++) {
+      sortItem[k].classList.remove('active');
+      sortItem[k].classList.add('delete');
+
+      if (sortItem[k].getAttribute('data-item') == targetData || targetData == "all") {
+        sortItem[k].classList.remove('delete');
+        sortItem[k].classList.add('active');
+      }
+    }
+  });
+}
+
+
+$(document).ready(function () {
+  $(".design-filter__link").click(function () {
+    var name = $(this).attr('data-filter');
+    if (name == "all") {
+      $(".shot-thumbnail").show('2000');
+    } else {
+      $(".shot-thumbnail").not("." + name).hide('2000');
+      $(".shot-thumbnail").filter("." + name).show('2000');
+    }
+  })
+
+  $(".design-filter li").click(function () {
+    $(this).addClass("active").siblings().removeClass("active");
+  })
+})
--- src/js/main.js
+++ src/js/main.js
@@ -1,16 +1,33 @@
 "use strict";
+
 $(function () {
+
   $('.hero-slider__inner').slick({
     dots: true,
     arrows: false,
     autoplay: true,
     autoplaySpeed: 6000
   })
+  //Product фильтр
+  var containerEl = document.querySelector('.product__list');
+  var mixer = mixitup(containerEl);
+  // ------------------------------------------------------------------------
+  //Design фильтр
+
 });
 
-// $(function () {
-//   // $('.hero-slider').slick({
-//   // dots: true,
+$(document).ready(function () {
+  $(".design-filter__item").click(function () {
+    var name = $(this).attr('data-filter-item');
+    if (name == "all") {
+      $(".design-catalog__item").show('2000');
+    } else {
+      $(".design-catalog__item").not("." + name).hide('2000');
+      $(".design-catalog__item").filter("." + name).show('2000');
+    }
+  })
 
-//   // });
-// })
+  $(".design-filter__item li").click(function () {
+    $(this).addClass("active").siblings().removeClass("active");
+  })
+})
